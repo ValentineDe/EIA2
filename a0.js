208adf439dd331e636e026679f81cb65cf960fba
Valentine <Valentine@Valentine-PC>
2017-03-19 15:54:51 +0100

    Aufgabe0

---
+++ a0.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var name;
+    name = prompt("Geben Sie bitte Ihren Namen ein:");
+    document.getElementById("Greetings").innerHTML += name;
+});
+//# sourceMappingURL=aufgabe1.js.map
