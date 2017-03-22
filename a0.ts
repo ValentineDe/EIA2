f2425f23a4fa19e5904685278244eb7eb855b339
ValentineDe <valentinedeinert95@gmx.de>
2017-03-22 22:40:42 +0100

    Delete a0.ts

--- a0.ts
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var name;
-    name = prompt("Geben Sie bitte Ihren Namen ein:");
-    document.getElementById("Greetings").innerHTML += name;
-});
