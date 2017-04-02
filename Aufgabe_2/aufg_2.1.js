e774ff56b2b2c869e5cb09ccff62000ee7fadf47
valen <valen@Pink-Lady.local>
2017-04-02 15:32:33 +0200

    Aufgabe 2.1 new

--- Aufgabe_2/aufg_2.1.js
+++ Aufgabe_2/aufg_2.1.js
@@ -1,18 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     let rice = 1;
+    let line = 0;
     for (let n = 0; n < 64; n++) {
         let element = document.createElement("div");
         element.textContent = rice.toString();
-        if (n % 2 == 0) {
-            element.className = "board white";
-        }
-        else {
-            element.className = "board black";
-        }
+        //        Umbruch nach jedem 8. Schachfeld
         if (n % 8 == 0) {
             element.className += " umbruch";
+            line = line + 1;
             console.log(n);
         }
+        if (line % 2 == 0) {
+            if (n % 2 != 0) {
+                element.className = "board black";
+            }
+            else {
+                element.className = "board white";
+            }
+        }
+        else {
+            if (n % 2 != 0) {
+                element.className = "board white";
+            }
+            else {
+                element.className = "board black";
+            }
+        }
+        //            Reis wird hinzugef�gt
         rice = rice * 2;
         document.body.appendChild(element);
     }
