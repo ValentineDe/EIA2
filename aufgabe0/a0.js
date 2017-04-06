986a719cda28ed8fc11c937507afd69675febd4f
valen <valen@Pink-Lady.fritz.box>
2017-04-06 08:53:01 +0200

    aufgabe0 neuer ordner

---
+++ aufgabe0/a0.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var name;
+    name = prompt("Geben Sie bitte Ihren Namen ein:");
+    document.getElementById("Greetings").innerHTML += name;
+});
+//# sourceMappingURL=a0.js.map
