f2afaccccf298f803d9a5dcdf8c7531df939975c
valen <valen@Pink-Lady.fritz.box>
2017-04-23 12:25:19 +0200

    Aufgabe4 fertig

--- aufgabe4/blumenwiese.js
+++ aufgabe4/blumenwiese.js
@@ -1,7 +1,7 @@
 //Aufgabe: Aufgabe 4
 //Name: Valentine Deinert
 //Matrikel: 254015
-//Datum: 18.04.2017  
+//Datum: 23.04.2017  
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
 //Er wurde nicht kopiert und auch nicht diktiert.
 var L4_Canvas;
@@ -17,33 +17,58 @@ var L4_Canvas;
         crc2.fillRect(0, 0, canvas.width, canvas.height);
         crc2.fillStyle = "#9DF2F6";
         crc2.fillRect(0, 0, canvas.width, 300);
-        //Berge, Sonne, Wolken    
+        //Berge, Sonne, Wolken, Baum, Blumen    
         drawMountain(0, 200, "#E4E4E4", "#C1C1C1");
         drawSun(370, 130, "#FFF555");
         drawCloud_1(400, 130, "white");
         drawCloud_2(55, 100, "#F2F2F2");
         drawTulip(100, 600, "#FF5735", "#FF856C");
         drawDaisy(300, 600);
-        drawTree(450, 320, "#206951", "#2B8B6B");
+        drawRose(100, 400);
+        //    drawSunflower(500, 600, "yellow", "orange");
+        drawTree(480, 290, "#206951", "#2B8B6B");
+        //Generiere random-Blumenwiese
+        for (var i = 0; i < 15; i++) {
+            let blumenWiese = Math.floor((Math.random() * 3) + 0); //random-Zahl + 0
+            let _x = Math.floor(Math.random() * (570 - 150)) + 150; //Blumen werden auf einer Breite von 420px generiert. Vom rechten Rand zum linken Rand.
+            let _y = Math.floor(Math.random() * (650 - 350)) + 350; //Blumen werden in einer Höhe bis zu 300 px generiert. Vom unteren Rand bis zu oberen Rand.
+            switch (blumenWiese) {
+                case 0:
+                    drawTulip(_x, _y, "#FF5735", "#FF856C");
+                    break;
+                case 1:
+                    drawRose(_x, _y);
+                    break;
+                case 2:
+                    drawDaisy(_x, _y);
+                    break;
+            }
+        }
     }
     //Berg       
     function drawMountain(_x, _y, _strokeColor, _fillColor) {
+        //Farbverlauf Mountain
+        var farbverlauf = crc2.createLinearGradient(0, 0, 0, 250);
+        farbverlauf.addColorStop("0.4", "white");
+        farbverlauf.addColorStop("0.5", "#C1C1C1");
+        farbverlauf.addColorStop("1", "#646464");
         crc2.beginPath();
-        crc2.fillStyle = _fillColor;
+        crc2.fillStyle = farbverlauf;
         crc2.strokeStyle = _strokeColor;
         crc2.lineWidth = 2;
-        crc2.moveTo(_x + 0, _y + 30);
-        crc2.lineTo(_x + 20, _y - 10);
-        crc2.lineTo(_x + 48, _y - 40);
-        crc2.lineTo(_x + 65, _y - 30);
-        crc2.lineTo(_x + 85, _y - 20);
-        crc2.lineTo(_x + 110, _y - 60);
-        crc2.lineTo(_x + 160, _y - 100);
+        crc2.moveTo(_x + 0, _y - 100);
+        crc2.lineTo(_x + 20, _y - 60);
+        crc2.lineTo(_x + 50, _y - 10);
+        crc2.lineTo(_x + 70, _y - 40);
+        crc2.lineTo(_x + 80, _y - 50);
+        crc2.lineTo(_x + 100, _y - 30);
+        crc2.lineTo(_x + 130, _y - 60);
+        crc2.lineTo(_x + 172, _y - 100);
         crc2.lineTo(_x + 190, _y - 130);
         crc2.lineTo(_x + 220, _y - 100);
         crc2.lineTo(_x + 250, _y - 60);
         crc2.lineTo(_x + 280, _y - 70);
-        crc2.lineTo(_x + 340, _y - 30);
+        crc2.lineTo(_x + 340, _y - 10);
         crc2.lineTo(_x + 400, _y - 20);
         crc2.lineTo(_x + 450, _y + 30);
         crc2.lineTo(_x + 470, _y + 70);
@@ -55,13 +80,13 @@ var L4_Canvas;
         //Linien im Berg  
         crc2.beginPath();
         crc2.strokeStyle = _strokeColor;
-        crc2.moveTo(_x + 85, _y - 20);
-        crc2.lineTo(_x + 110, _y + 10);
-        crc2.lineTo(_x + 140, _y + 30);
+        crc2.moveTo(_x + 100, _y - 30);
+        crc2.lineTo(_x + 140, _y + 10);
+        crc2.lineTo(_x + 150, _y + 30);
         crc2.moveTo(_x + 250, _y - 60);
         crc2.lineTo(_x + 220, _y - 40);
-        crc2.moveTo(_x + 340, _y - 30);
-        crc2.lineTo(_x + 320, _y + 0);
+        crc2.moveTo(_x + 340, _y - 10);
+        crc2.lineTo(_x + 320, _y + 10);
         crc2.lineTo(_x + 290, _y + 20);
         crc2.lineTo(_x + 250, _y + 100);
         crc2.stroke();
@@ -74,7 +99,7 @@ var L4_Canvas;
         crc2.closePath();
         crc2.fill();
     }
-    //Baum
+    //Baum    
     function drawTree(_x, _y, _strokeColor, _fillColor) {
         //Stamm
         crc2.beginPath();
@@ -109,7 +134,7 @@ var L4_Canvas;
         crc2.fill();
         crc2.stroke();
     }
-    //Wolken 
+    //Wolke groß   
     function drawCloud_1(_x, _y, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
@@ -128,6 +153,7 @@ var L4_Canvas;
         crc2.closePath();
         crc2.fill();
     }
+    //Wolke klein
     function drawCloud_2(_x, _y, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
@@ -146,6 +172,7 @@ var L4_Canvas;
         crc2.closePath();
         crc2.fill();
     }
+    //Tulpe
     function drawTulip(_x, _y, _strokeColor, _fillColor) {
         //Stängel
         crc2.beginPath();
@@ -184,6 +211,7 @@ var L4_Canvas;
         crc2.stroke();
         crc2.closePath();
     }
+    //Gänseblümchen
     function drawDaisy(_x, _y) {
         //Stängel
         crc2.beginPath();
@@ -229,6 +257,121 @@ var L4_Canvas;
         crc2.beginPath();
         crc2.fillStyle = "#F5B700";
         crc2.arc(_x + 2, _y - 50, 10, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+    }
+    //Rose
+    function drawRose(_x, _y) {
+        //Stängel
+        crc2.beginPath();
+        crc2.fillStyle = "#03AF00";
+        crc2.fillRect(_x, _y - 40, 4, 40);
+        crc2.closePath();
+        crc2.fill();
+        //Blätter  
+        crc2.fillStyle = "#89FF87";
+        crc2.arc(_x + 2, _y - 29, 15, 0, 1.5);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#64D562";
+        crc2.arc(_x + 17, _y - 13, 15, 3, -1.5);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#64D562";
+        crc2.arc(_x - 10, _y - 12, 11, 4.1, 0.4);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#89FF87";
+        crc2.arc(_x - 2, _y - 19, 13, 1.3, -3);
+        crc2.closePath();
+        crc2.fill();
+        //Blüte
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 2, _y - 56, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 2, _y - 33, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 15, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 14, _y - 37, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x - 10, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x - 9, _y - 37, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x - 3, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x + 7, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x - 3, _y - 39, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x + 7, _y - 39, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x - 8, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x + 12, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x + 2, _y - 48, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x + 2, _y - 42, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x + 6, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x - 2, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#596BDB";
+        crc2.arc(_x + 2, _y - 45, 5, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.fill();
     }
--- aufgabe4/blumenwiese.ts
+++ aufgabe4/blumenwiese.ts
@@ -1,7 +1,7 @@
 //Aufgabe: Aufgabe 4
 //Name: Valentine Deinert
 //Matrikel: 254015
-//Datum: 18.04.2017  
+//Datum: 23.04.2017  
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
 //Er wurde nicht kopiert und auch nicht diktiert.
 
@@ -9,48 +9,75 @@ namespace L4_Canvas {
     window.addEventListener("load", init);
     let crc2: CanvasRenderingContext2D;
 
-function init(_event: Event): void {
+    function init(_event: Event): void {
 
-    let canvas: HTMLCanvasElement;
-    canvas = document.getElementsByTagName("canvas")[0];
-    
-//Himmel und Wiese    
-    crc2= canvas.getContext("2d");
-    crc2.fillStyle = "#56D785";
-    crc2.fillRect(0, 0, canvas.width, canvas.height);
-    crc2.fillStyle = "#9DF2F6";
-    crc2.fillRect(0, 0, canvas.width, 300);
-    
-//Berge, Sonne, Wolken    
-    drawMountain(0, 200, "#E4E4E4", "#C1C1C1");
-    drawSun(370, 130, "#FFF555"); 
-    drawCloud_1(400, 130, "white");
-    drawCloud_2(55, 100, "#F2F2F2");
-    drawTulip(100, 600, "#FF5735", "#FF856C");
-    drawDaisy(300,600);
-    drawTree(450, 320, "#206951", "#2B8B6B");
+        let canvas: HTMLCanvasElement;
+        canvas = document.getElementsByTagName("canvas")[0];
 
-}
-  
-//Berg       
-function drawMountain (_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+        //Himmel und Wiese    
+        crc2 = canvas.getContext("2d");
+        crc2.fillStyle = "#56D785";
+        crc2.fillRect(0, 0, canvas.width, canvas.height);
+        crc2.fillStyle = "#9DF2F6";
+        crc2.fillRect(0, 0, canvas.width, 300);
+
+        //Berge, Sonne, Wolken, Baum, Blumen    
+        drawMountain(0, 200, "#E4E4E4", "#C1C1C1");
+        drawSun(370, 130, "#FFF555");
+        drawCloud_1(400, 130, "white");
+        drawCloud_2(55, 100, "#F2F2F2");
+        drawTulip(100, 600, "#FF5735", "#FF856C");
+        drawDaisy(300, 600);
+        drawRose(100, 400);
+        //    drawSunflower(500, 600, "yellow", "orange");
+        drawTree(480, 290, "#206951", "#2B8B6B");
+
+        //Generiere random-Blumenwiese
+        for (var i: number = 0; i < 15; i++) { //Insgesamt 15 Durchläufe -> 15 Blumen random generieren
+            let blumenWiese: number = Math.floor((Math.random() * 3) + 0); //random-Zahl + 0
+            let _x: number = Math.floor(Math.random() * (570 - 150)) + 150; //Blumen werden auf einer Breite von 420px generiert. Vom rechten Rand zum linken Rand.
+            let _y: number = Math.floor(Math.random() * (650 - 350)) + 350; //Blumen werden in einer Höhe bis zu 300 px generiert. Vom unteren Rand bis zu oberen Rand.
+
+            switch (blumenWiese) {
+                case 0:
+                    drawTulip(_x, _y, "#FF5735", "#FF856C");
+                    break;
+                case 1:
+                    drawRose(_x, _y);
+                    break;
+                case 2:
+                    drawDaisy(_x, _y);
+                    break;
+            }
+        }
+    }
+    //Berg       
+    function drawMountain(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+        
+        //Farbverlauf Mountain
+        var farbverlauf: any = crc2.createLinearGradient(0, 0, 0, 250);
+        farbverlauf.addColorStop("0.4", "white");
+        farbverlauf.addColorStop("0.5", "#C1C1C1");
+        farbverlauf.addColorStop("1", "#646464");
+
         crc2.beginPath();
-        crc2.fillStyle = _fillColor;
+        crc2.fillStyle = farbverlauf;
         crc2.strokeStyle = _strokeColor;
         crc2.lineWidth = 2;
-  
-        crc2.moveTo(_x + 0, _y + 30);
-        crc2.lineTo(_x + 20, _y - 10);
-        crc2.lineTo(_x + 48, _y - 40);
-        crc2.lineTo(_x + 65, _y - 30);
-        crc2.lineTo(_x + 85, _y - 20);
-        crc2.lineTo(_x + 110, _y - 60);
-        crc2.lineTo(_x + 160, _y - 100);
+
+        crc2.moveTo(_x + 0, _y - 100);
+        crc2.lineTo(_x + 20, _y - 60);
+        crc2.lineTo(_x + 50, _y - 10);
+        crc2.lineTo(_x + 70, _y - 40);
+        crc2.lineTo(_x + 80, _y - 50);
+        crc2.lineTo(_x + 100, _y - 30);
+        crc2.lineTo(_x + 130, _y - 60);
+        crc2.lineTo(_x + 172, _y - 100);
         crc2.lineTo(_x + 190, _y - 130);
         crc2.lineTo(_x + 220, _y - 100);
         crc2.lineTo(_x + 250, _y - 60);
         crc2.lineTo(_x + 280, _y - 70);
-        crc2.lineTo(_x + 340, _y - 30);
+        crc2.lineTo(_x + 340, _y - 10);
         crc2.lineTo(_x + 400, _y - 20);
         crc2.lineTo(_x + 450, _y + 30);
         crc2.lineTo(_x + 470, _y + 70);
@@ -59,42 +86,38 @@ function drawMountain (_x: number, _y: number, _strokeColor: string, _fillColor:
         crc2.fill();
         crc2.stroke();
         crc2.closePath();
-    
-//Linien im Berg  
+        //Linien im Berg  
         crc2.beginPath();
         crc2.strokeStyle = _strokeColor;
-        crc2.moveTo(_x + 85, _y - 20);
-        crc2.lineTo(_x + 110, _y + 10);
-        crc2.lineTo(_x + 140, _y + 30);
+        crc2.moveTo(_x + 100, _y - 30);
+        crc2.lineTo(_x + 140, _y + 10);
+        crc2.lineTo(_x + 150, _y + 30);
         crc2.moveTo(_x + 250, _y - 60);
         crc2.lineTo(_x + 220, _y - 40);
-        crc2.moveTo(_x + 340, _y - 30);
-        crc2.lineTo(_x + 320, _y + 0);
+        crc2.moveTo(_x + 340, _y - 10);
+        crc2.lineTo(_x + 320, _y + 10);
         crc2.lineTo(_x + 290, _y + 20);
         crc2.lineTo(_x + 250, _y + 100);
-        crc2.stroke();   
-}
-//Sonne  
-function drawSun (_x: number, _y: number, _fillColor: string): void {  
+        crc2.stroke();
+    }
+    //Sonne  
+    function drawSun(_x: number, _y: number, _fillColor: string): void {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.arc(_x - 15, _y - 65, 50, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.fill();
-}  
-  
-//Baum
-    
-function drawTree (_x: number, _y: number,_strokeColor: string, _fillColor: string): void {  
-        
-    //Stamm
+    }
+    //Baum    
+    function drawTree(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+
+        //Stamm
         crc2.beginPath();
         crc2.fillStyle = "#706B57";
         crc2.fillRect(_x, _y, 20, 40);
         crc2.closePath();
         crc2.fill();
-    
-      //Nadeln
+        //Nadeln
         crc2.fillStyle = _fillColor;
         crc2.beginPath();
         crc2.strokeStyle = _strokeColor;
@@ -102,36 +125,34 @@ function drawTree (_x: number, _y: number,_strokeColor: string, _fillColor: stri
         crc2.lineTo(_x - 40, _y + 0);
         crc2.lineTo(_x + 10, _y - 60);
         crc2.closePath();
-        crc2.fill()
+        crc2.fill();
         crc2.stroke();
-    
+
         crc2.beginPath();
         crc2.strokeStyle = _strokeColor;
         crc2.moveTo(_x + 55, _y - 30);
         crc2.lineTo(_x - 32, _y - 30);
         crc2.lineTo(_x + 10, _y - 70);
         crc2.closePath();
-        crc2.fill()
-        crc2.stroke(); 
-    
+        crc2.fill();
+        crc2.stroke();
+
         crc2.beginPath();
         crc2.strokeStyle = _strokeColor;
         crc2.moveTo(_x + 45, _y - 55);
         crc2.lineTo(_x - 23, _y - 55);
         crc2.lineTo(_x + 10, _y - 100);
         crc2.closePath();
-        crc2.fill()
-        crc2.stroke(); 
-        
-}      
-    
-//Wolken 
-    
-function drawCloud_1(_x: number, _y: number, _fillColor: string): void {   
-       
+        crc2.fill();
+        crc2.stroke();
+
+    }
+    //Wolke groß   
+    function drawCloud_1(_x: number, _y: number, _fillColor: string): void {
+
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
-    
+
         crc2.arc(_x - 15, _y - 65, 18, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.arc(_x - 18, _y - 40, 20, 0, 2 * Math.PI);
@@ -146,13 +167,13 @@ function drawCloud_1(_x: number, _y: number, _fillColor: string): void {
         crc2.arc(_x + 15, _y - 40, 28, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.fill();
-}    
-    
-function drawCloud_2(_x: number, _y: number, _fillColor: string): void {   
-       
+    }
+    //Wolke klein
+    function drawCloud_2(_x: number, _y: number, _fillColor: string): void {
+
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
-    
+
         crc2.arc(_x - 23, _y - 55, 10, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.arc(_x - 25, _y - 40, 12, 0, 2 * Math.PI);
@@ -167,17 +188,17 @@ function drawCloud_2(_x: number, _y: number, _fillColor: string): void {
         crc2.arc(_x - 3, _y - 45, 20, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.fill();
-} 
-    
-function drawTulip(_x: number, _y: number,_strokeColor: string, _fillColor: string): void {   
-        
-    //Stängel
+    }
+    //Tulpe
+    function drawTulip(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+
+        //Stängel
         crc2.beginPath();
         crc2.fillStyle = "#579200";
         crc2.fillRect(_x, _y - 40, 4, 40);
         crc2.closePath();
         crc2.fill();
-    //Blätter  
+        //Blätter  
         crc2.fillStyle = "#457200";
         crc2.arc(_x - 1, _y - 29, 30, 0, 1.5);
         crc2.closePath();
@@ -187,13 +208,13 @@ function drawTulip(_x: number, _y: number,_strokeColor: string, _fillColor: stri
         crc2.arc(_x - 3, _y - 18, 18, 1.3, - 3);
         crc2.closePath();
         crc2.fill();
-    //Blüte
+        //Blüte
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.arc(_x + 1, _y - 58, 18, 0, - 3.1);
         crc2.closePath();
         crc2.fill();
-    
+
         crc2.beginPath();
         crc2.strokeStyle = _strokeColor;
         crc2.moveTo(_x - 16, _y - 58);
@@ -208,17 +229,17 @@ function drawTulip(_x: number, _y: number,_strokeColor: string, _fillColor: stri
         crc2.fill();
         crc2.stroke();
         crc2.closePath();
-}     
-    
-function drawDaisy(_x: number, _y: number): void{
-       
-      //Stängel
+    }
+    //Gänseblümchen
+    function drawDaisy(_x: number, _y: number): void {
+
+        //Stängel
         crc2.beginPath();
         crc2.fillStyle = "#185A18";
         crc2.fillRect(_x, _y - 43, 4, 60);
         crc2.closePath();
         crc2.fill();
-      //Blätter  
+        //Blätter  
         crc2.fillStyle = "#337245";
         crc2.arc(_x - 1, _y - 29, 30, 0, 1.5);
         crc2.closePath();
@@ -238,9 +259,9 @@ function drawDaisy(_x: number, _y: number): void{
         crc2.arc(_x - 3, _y - 30, 18, 1.3, - 3);
         crc2.closePath();
         crc2.fill();
-     //Blüte
+        //Blüte
         crc2.beginPath();
-        crc2.fillStyle = "white"; 
+        crc2.fillStyle = "white";
         crc2.arc(_x - 10, _y - 50, 12, 0, 2 * Math.PI);
         crc2.closePath();
         crc2.fill();
@@ -255,15 +276,176 @@ function drawDaisy(_x: number, _y: number): void{
         crc2.fill();
 
         crc2.beginPath();
-        crc2.fillStyle = "#F5B700";  
+        crc2.fillStyle = "#F5B700";
         crc2.arc(_x + 2, _y - 50, 10, 0, 2 * Math.PI);
         crc2.closePath();
-        crc2.fill()
-    
-   
-        
+        crc2.fill();
+    }
+    //Rose
+    function drawRose(_x: number, _y: number): void {
 
-       
-        
-}
-}
+        //Stängel
+        crc2.beginPath();
+        crc2.fillStyle = "#03AF00";
+        crc2.fillRect(_x, _y - 40, 4, 40);
+        crc2.closePath();
+        crc2.fill();
+        //Blätter  
+        crc2.fillStyle = "#89FF87";
+        crc2.arc(_x + 2, _y - 29, 15, 0, 1.5);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#64D562";
+        crc2.arc(_x + 17, _y - 13, 15, 3, -1.5);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#64D562";
+        crc2.arc(_x - 10, _y - 12, 11, 4.1, 0.4);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#89FF87";
+        crc2.arc(_x - 2, _y - 19, 13, 1.3, - 3);
+        crc2.closePath();
+        crc2.fill();
+        //Blüte
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 2, _y - 56, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 2, _y - 33, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 15, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x + 14, _y - 37, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x - 10, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#C4C8E1";
+        crc2.arc(_x - 9, _y - 37, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x - 3, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x + 7, _y - 50, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x - 3, _y - 39, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x + 7, _y - 39, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x - 8, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#9FA7DC";
+        crc2.arc(_x + 12, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x + 2, _y - 48, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x + 2, _y - 42, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x + 6, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.beginPath();
+        crc2.fillStyle = "#7C89DF";
+        crc2.arc(_x - 2, _y - 45, 8, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+
+        crc2.beginPath();
+        crc2.fillStyle = "#596BDB";
+        crc2.arc(_x + 2, _y - 45, 5, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+    }
+
+    //function drawSunflower(_x: number, _y: number, _strokeColor: string, _fillColor: string): void{  
+    //Stängel
+    //        crc2.beginPath();
+    //        crc2.fillStyle = "#185A18";
+    //        crc2.fillRect(_x, _y - 41, 6, 80);
+    //        crc2.closePath();
+    //        crc2.fill();
+    //Blätter  
+    //        crc2.fillStyle = "#337245";
+    //        crc2.arc(_x + 0, _y + 5, 35, 0, 1.5);
+    //        crc2.closePath();
+    //        crc2.fill();
+    //        crc2.beginPath();
+    //        crc2.fillStyle = "#185A18";
+    //        crc2.arc(_x + 32, _y + 36, 30, 3, -1.5);
+    //        crc2.closePath();
+    //        crc2.fill();
+    //        crc2.beginPath();
+    //        crc2.fillStyle = "#185A18";
+    //        crc2.arc(_x - 22, _y + 30, 24, 4, 0.4);
+    //        crc2.closePath();
+    //        crc2.fill();
+    //        crc2.beginPath();
+    //        crc2.fillStyle = "#337245";
+    //        crc2.arc(_x - 9, _y + 15, 26, 1, - 3);
+    //        crc2.closePath();
+    //        crc2.fill();
+    //Blüte
+    //        crc2.lineWidth = 1;
+    //        crc2.fillStyle = _fillColor;
+    //        crc2.strokeStyle = _strokeColor;
+    //        crc2.beginPath();
+    //        crc2.moveTo(_x - 5, _y - 50);
+    //        crc2.lineTo(_x + 10, _y - 100);
+    //     lineTo(_x + 10, _y - 50);
+    //        crc2.lineTo(_x + 20, _y - 100);
+    //        crc2.lineTo(_x + 30, _y - 50);
+
+    //        crc2.fill();
+    //        crc2.stroke();
+    //        crc2.closePath(); 
+    //    
+    //        crc2.beginPath();
+    //        crc2.fillStyle = "#726D3F";  
+    //        crc2.arc(_x + 2, _y - 48, 30, 0, 2 * Math.PI);
+    //        crc2.closePath();
+    //        crc2.fill() 
+    //   }
+
+} 
